1bb06764603f6bbd08c8129c35954a68ccca9995
Ebadur Rahman <ebadurshohag@gmail.com>
2025-04-08 05:11:12 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import "./App.css";
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello World</h1>
+      <button className="btn btn-primary">Primary</button>
+    </>
+  );
+}
+
+export default App;
